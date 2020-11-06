60c6d3b631f934ab6007f1d83872eee057082e72
clonetech <kencheruiyot.kc@gmail.com>
2020-11-06 17:28:55 +0300

    Added files and changes required for deployment to heroku

--- palsbet/settings.py
+++ palsbet/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+from django.contrib import admin
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -129,6 +130,16 @@ STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Heroku: Update database configuration from $DATABASE_URL.
-import dj_database_url
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
+
+LOGIN_REDIRECT_URL = '/home'
+
+LOGOUT_REDIRECT_URL = '/viptips'
+
+if 'DATABASE_URL' in os.environ:
+    import dj_database_url
+    DATABASES = {'default': dj_database_url.config()}
+
+#admin.site.site_header = 'PalsBet Admin Panel'
+#admin.site.site_title = 'PalsBet Admin Panel'
+
+ADMINS = (('Keneth', 'palsbet1@gmail.com'),)
